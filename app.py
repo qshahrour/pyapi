24800c162832b10222c4f90eb7e9b032329dc1cb
qshahrour <109384983+qshahrour@users.noreply.github.com>
2026-01-18 09:54:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ import os
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 db = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
