627a14ecde7d3375bafce770cfa6fae797c9c958
qshahrour <109384983+qshahrour@users.noreply.github.com>
2026-01-23 20:57:54 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from database import SessionLocal, engine
+from models import Base, User
+from fastapi.middleware.cors import CORSMiddleware
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.get("/api/users")
+def get_users(db: Session = Depends(get_db)):
+    return db.query(User).all()
+
+@app.post("/api/users")
+def create_user(name: str, db: Session = Depends(get_db)):
+    user = User(name=name)
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+    return user
